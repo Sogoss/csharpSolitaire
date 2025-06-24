2ba57f32505b033701277364012a853e462e745b
Sogoss <143703253+Sogoss@users.noreply.github.com>
2025-06-24 18:20:53 +0200

    Created CardStack, ICardStack

---
+++ csharpSolitaire/objects/CardStackObject/CardStack.cs
@@ -0,0 +1,24 @@
+﻿using csharpSolitaire.objects.CardObject;
+
+namespace csharpSolitaire.objects.CardStackObject;
+
+internal class CardStack() : ICardStack {
+    public Stack<Card> cardsStack = new();
+
+    public void Print() {
+        foreach (Card card in cardsStack)
+            card.Print();
+    }
+
+    public void Pop(Card card) {
+        if (card == null) return;
+        else if (cardsStack.Contains(card)) return;
+        else cardsStack.Pop();
+    }
+
+    public void Push(Card card) {
+        if (card == null) return;
+        else if (cardsStack.Contains(card)) return;
+        else cardsStack.Push(card);
+    }
+}
---
+++ csharpSolitaire/objects/CardStackObject/ICardStack.cs
@@ -0,0 +1,8 @@
+﻿using csharpSolitaire.objects.CardObject;
+
+namespace csharpSolitaire.objects.CardStackObject;
+internal interface ICardStack {
+    void Pop(Card card);
+    void Push(Card card);
+    void Print();
+}
